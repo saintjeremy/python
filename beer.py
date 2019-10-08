418583d8de4eb803fd4be093055576c8a3010964
saintjeremy <saintjeremy@gmail.com>
2019-10-08 13:28:03 -0700

    update beer.py with working syntax

--- beer.py
+++ beer.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
+# beer song
+
 #i = range (99, 0, -1)
 #print i,"bottles of beer ",i,"beer"
 
 
-for i in range (99, 0, -1): 
-	print i,"bottles of beeron the wall.",i,"bottles of beer"
+for i in range (99, 0, -1):
+# for n in range (98, 0, -1):
+	print i,"bottles of beer on the wall.",i,"bottles of beer."
+	print "Take one down, pass it around",i -1,"bottles of beer on the wall"
